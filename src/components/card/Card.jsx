b9e7f57ba630e0eda26dba28fba13131832ff215
SDatgit20 <srashtideshmukh2000@gmail.com>
2024-05-06 00:43:23 +0530

    Refactor. item card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,8 +9,7 @@ class Card extends Component {
         return (
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">Item ID: {id}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Item Name: {name}</h6>
+                    <h6 className="card-title mb-2 text-muted">Item Name: {name}</h6>
                     <p className="card-text">Description: {description}</p>
                     {imagePath && <img src={imagePath} className="card-img-top" alt="Item Image" />}
                 </div>
